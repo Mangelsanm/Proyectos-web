f81f8960f9739ea3e18a9db0baa1c30241dbd091
Miguel Mares <mares112358@gmail.com>
2021-04-17 16:38:05 -0500

    Animacion Menu Bar, para vista de celular o tablet

---
+++ Animacion Menu Bar/main.js
@@ -0,0 +1,14 @@
+// setInterval(function() { 
+//     $('.menu-container').toggleClass('is-menu-open') 
+//   }, 1500);
+
+// const btnMenu = document.querySelector(".menu-container");
+
+// btnMenu.addEventListener("click", function(){
+//   btnMenu.classList.toggle("is-menu-open");
+// })
+
+const btnMenu = document.querySelector(".menu-container");
+btnMenu.addEventListener("click", function() {
+  btnMenu.classList.toggle("is-menu-open");
+})
